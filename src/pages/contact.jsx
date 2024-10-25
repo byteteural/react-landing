2191a0da1e9f26387e69067424035834973b3172
UdomUdom <udomzaark@gmail.com>
2024-10-25 18:50:09 +0700

    delete image

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,10 @@
 export default function Contact() {
   return (
     <div className="bg-dark flex items-center justify-center min-h-full px-8 py-4">
+      {/* บริษัท เอ็น วี พี พร็อพเพอร์ตี้ จำกัด สำนักงานใหญ่
+72/51 หมู่ที่ 2 ถนนบางกรวย-ไทรน้อย ตำบลบางบัวทอง
+อำเภอบางบัวทอง จังหวัด นนทบุรี 11110
+โทรศัพท์ 02-149-9304, 086-554-7966 E-mail kisttisak66@gmail.com */}
       Contact
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur autem reprehenderit rerum voluptate, tempora doloremque magni blanditiis, perspiciatis quas suscipit deleniti. Pariatur minus quas quidem, dolor maxime dolores sunt totam?
     </div>
